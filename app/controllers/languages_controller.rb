85bc15b16f686f763b6760bdda19c72a0a1b3014
Gus Shaw Stewart <g.shawstewart@gmail.com>
2016-06-06 13:00:04 +0100

    interest in the lessons now appears when each lesson shown

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -33,6 +33,7 @@ class LanguagesController < ApplicationController
   # GET /languages/1.json
   def show
     @language = Language.find(params[:id])
+    @interest = Language.all
   end
 
   # GET /languages/new
