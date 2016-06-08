fc3b2f285d79c87bd925113c2143eb04662d8788
Gus Shaw Stewart <g.shawstewart@gmail.com>
2016-06-08 10:45:16 +0100

    working match-maker

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,4 +1,18 @@
 class StaticsController < ApplicationController
+  
+
   def homepage
+      
+      @teachers = User.find_by_sql ["SELECT us.* FROM languages_users u
+                        LEFT JOIN learning le ON le.language_id = u.language_id
+                        LEFT JOIN users us ON us.id = le.user_id
+                        WHERE u.user_id = ?" , current_user.id]
+
+      @learners = User.find_by_sql ["SELECT us.* FROM learning le
+                        LEFT JOIN languages_users u ON le.language_id = u.language_id
+                        LEFT JOIN users us ON us.id = u.user_id
+                        WHERE le.user_id = ?" , current_user.id]
+
   end
+
 end
